fb3519fac781005539f7c0bac1057b9ffa9063ec
Milan Stevanovic <milanste999@gmail.com>
2021-05-24 13:56:50 +0200

    V1 Read Analog Out / Read Analog In

--- dCom/Modbus/ModbusFunctions/ReadInputRegistersFunction.cs
+++ dCom/Modbus/ModbusFunctions/ReadInputRegistersFunction.cs
@@ -24,15 +24,34 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc />
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusReadCommandParameters mrcp = this.CommandParameters as ModbusReadCommandParameters;
+            byte[] niz = new byte[12];
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)mrcp.TransactionId)), 0, niz, 0, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)mrcp.ProtocolId)), 0, niz, 2, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)mrcp.Length)), 0, niz, 4, 2);
+
+            niz[6] = mrcp.UnitId;
+            niz[7] = mrcp.FunctionCode;
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)mrcp.StartAddress)), 0, niz, 8, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)mrcp.Quantity)), 0, niz, 10, 2);
+
+            return niz;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusReadCommandParameters mrcp = this.CommandParameters as ModbusReadCommandParameters;
+            Dictionary<Tuple<PointType, ushort>, ushort> dictionary = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            byte port1 = response[10];
+            byte port2 = response[9];
+
+            ushort value = (ushort)(port1 + (port2 << 8));
+            dictionary.Add(new Tuple<PointType, ushort>(PointType.ANALOG_INPUT, mrcp.StartAddress), value);
+
+            return dictionary;
         }
     }
 }
